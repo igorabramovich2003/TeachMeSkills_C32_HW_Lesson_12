757aca68300fb869b4b961f77360e3150270394b
Igor Abramovich <igorab2033@gmail.com>
2024-11-10 15:39:54 +0300

    init commit

--- src/com/teachmeskills/lesson_12/ApplicationRunner.java
+++ src/com/teachmeskills/lesson_12/ApplicationRunner.java
@@ -2,7 +2,16 @@ package com.teachmeskills.lesson_12;
 
 import com.teachmeskills.lesson_12.validator.HexCodeValidator;
 import com.teachmeskills.lesson_12.validator.URLValidator;
+/*
+Task 1
+Write a program that the passed string is a hex code for a color description.
+For example, when entering the value #8b2323 into the program, the program should return - Yes.
 
+Task 2
+Write a program that will check that the entered link is valid.
+For example, when analyzing a link like this https://teachmeskills.by/kursy/programmirovaniya-online - the program should return that the link is valid.
+and when entering tcp://teachmeskills.by/kursy/programmirovaniya-online - the program should return that the link is not valid (the link starts with neither http nor https)
+ */
 public class ApplicationRunner {
     public static void main(String[] args) {
         //Task1
